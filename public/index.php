8d91297f2a25affac837556577b275fed9a90a31
VitorSanto-ux <vitor.h.santo@aluno.senai.br>
2026-03-11 14:54:27 -0300

    base

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início</title>
+</head>
+<body>
+    <div>
+      <img src="public/assets/img/logo_nextlevel.png" alt="">
+    </div>
+
+    <div>
+        <h1>Next Level</h1>
+
+        <nav>
+            <ul>
+                <li><a href="">Cadastrar Evento</a></li>
+                <li><a href="">Cadastrar Participantes</a></li>
+                <li><a href="">Sobre</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
